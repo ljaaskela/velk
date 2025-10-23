909ecb1232259450eacc33b4830d894dc9f6bee4
ljaaskela <lauri.jaaskela@iki.fi>
2025-10-23 21:01:14 +0300

    Better GetName<T>()

--- ltk/common.h
+++ ltk/common.h
@@ -29,32 +29,48 @@ constexpr Uid MakeHash(const std::string_view toHash)
     return result;
 }
 
-template<class... T>
+template<std::size_t... Idxs>
+constexpr auto substring_as_array(std::string_view str, std::index_sequence<Idxs...>)
+{
+    return std::array{str[Idxs]..., '\0'};
+}
+
+template<typename T>
+constexpr auto type_name_array()
+{
+#if defined(__clang__)
+    constexpr auto prefix = std::string_view{"[T = "};
+    constexpr auto suffix = std::string_view{"]"};
+    constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(__GNUC__)
+    constexpr auto prefix = std::string_view{"with T = "};
+    constexpr auto suffix = std::string_view{"]"};
+    constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(_MSC_VER)
+    constexpr auto prefix = std::string_view{"type_name_array<"};
+    constexpr auto suffix = std::string_view{">(void)"};
+    constexpr auto function = std::string_view{__FUNCSIG__};
+#else
+#error Unsupported compiler
+#endif
+    constexpr auto start = function.find(prefix) + prefix.size();
+    constexpr auto end = function.rfind(suffix);
+    static_assert(start < end);
+    constexpr auto name = function.substr(start, (end - start));
+    return substring_as_array(name, std::make_index_sequence<name.size()>{});
+}
+
+template<typename T>
+struct type_name_holder
+{
+    static inline constexpr auto value = type_name_array<T>();
+};
+
+template<typename T>
 constexpr std::string_view GetName()
 {
-#ifdef _MSC_VER
-    char const *p = __FUNCSIG__;
-#else
-    char const *p = __PRETTY_FUNCTION__;
-#endif
-    while (*p++ != '=');
-    for (; *p == ' '; ++p);
-    char const* p2 = p;
-    int count = 1;
-    for (;;++p2)
-    {
-        switch (*p2)
-        {
-        case '[':
-            ++count;
-            break;
-        case ']':
-            --count;
-            if (!count)
-                return {p, std::size_t(p2 - p)};
-        }
-    }
-    return {};
+    constexpr auto &value = type_name_holder<T>::value;
+    return std::string_view{value.data(), value.size()};
 }
 
 template<class... T>
